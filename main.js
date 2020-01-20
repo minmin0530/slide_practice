70ed0dbef1da01163387d11e647373a8cca74103
Yoshiki Izumi <continue_jump@icloud.com>
2020-01-20 15:00:11 +0900

    #1 slide section css

--- main.js
+++ main.js
@@ -1,37 +1,21 @@
 window.onload = function() {
-    // const mainvisual = this.document.getElementsByClassName("mainvisual");
-    // const mainvisual_text = this.document.getElementsByClassName("mainvisual_text");
-    // let left0 = 0;
-    // let left1 = 0;
-
-    // loop();
-
-    // function loop() {
-    //     left0 -= 1;
-        // mainvisual[0].style.left = left0 + "px";
-        // mainvisual_text[0].style.left = left0 + "px";
-        // mainvisual[0].style.width = -left0 + "px";
-        // mainvisual_text[0].style.width = -left0 + "px";
-
-    //     requestAnimationFrame(loop);
-    // };
-
     window.addEventListener( "scroll", function () {
         const scrollTop = document.scrollingElement.scrollTop;
-        document.getElementsByClassName("article__section_img2")[0].style.left = 1366 - scrollTop + "px";
-        document.getElementsByClassName("article__section_img4")[0].style.left = 1366 - scrollTop + "px";
-        if (scrollTop <= 430) {
-            document.getElementsByClassName("article__section_img1")[0].style.width = scrollTop + "px";
-            document.getElementsByClassName("article__section_img2")[0].style.width = scrollTop + "px";
-            document.getElementsByClassName("article__section_img3")[0].style.width = scrollTop + "px";
-            document.getElementsByClassName("article__section_img4")[0].style.width = scrollTop + "px";
-        } else {
-            document.getElementsByClassName("article__section_img1")[0].style.width = 430 + "px";
-            document.getElementsByClassName("article__section_img2")[0].style.width = 430 + "px";
-            document.getElementsByClassName("article__section_img3")[0].style.width = 430 + "px";
-            document.getElementsByClassName("article__section_img4")[0].style.width = 430 + "px";
+        if (scrollTop <= 700) {
             document.getElementsByClassName("article__section_img1")[0].style.left = scrollTop - 430 + "px";
-            document.getElementsByClassName("article__section_img3")[0].style.left = scrollTop - 430 + "px";
+            document.getElementsByClassName("article__section_text1")[0].style.left = scrollTop + 50 + "px";
+        }
+        if (700 <= scrollTop && scrollTop <= 1000) {
+            document.getElementsByClassName("article__section_text2")[0].style.left = 1366 - scrollTop + "px";
+            document.getElementsByClassName("article__section_img2")[0].style.left = 1366 + 300 - scrollTop + "px";
+        }
+        if (1300 <= scrollTop && scrollTop <= 1600) {
+            document.getElementsByClassName("article__section_img3")[0].style.left = scrollTop + 50 - 1350 + "px";
+            document.getElementsByClassName("article__section_text3")[0].style.left = scrollTop - 430 - 400 + "px";
+        }
+        if (1700 <= scrollTop && scrollTop <= 2000) {
+            document.getElementsByClassName("article__section_text4")[0].style.left = 1366 + 1000 - scrollTop + "px";
+            document.getElementsByClassName("article__section_img4")[0].style.left = 1366 + 1300 - scrollTop + "px";
         }
     }, false);
 }
